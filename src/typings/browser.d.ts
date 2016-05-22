b1b129db16653cbe23baa8d49d0f5051061784d9
David Sherret <dsherret@gmail.com>
2016-05-21 23:54:09 -0400

    Upgrade to typings 1.0

--- src/typings/browser.d.ts
+++
@@ -1,4 +0,0 @@
-/// <reference path="browser/ambient/mocha/index.d.ts" />
-/// <reference path="browser/ambient/node/index.d.ts" />
-/// <reference path="browser/ambient/tmp/index.d.ts" />
-/// <reference path="browser/definitions/code-block-writer/index.d.ts" />
---
+++ src/typings/index.d.ts
@@ -0,0 +1,3 @@
+/// <reference path="globals/mocha/index.d.ts" />
+/// <reference path="globals/node/index.d.ts" />
+/// <reference path="globals/tmp/index.d.ts" />
