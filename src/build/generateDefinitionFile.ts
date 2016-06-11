0fce0c290eb7b88a9778eb032b64f7f4ba521d92
David Sherret <dsherret@gmail.com>
2016-06-11 12:46:00 -0400

    Remove old unnecessary code used when generating definition file

--- src/build/generateDefinitionFile.ts
+++ src/build/generateDefinitionFile.ts
@@ -8,22 +8,6 @@ export function generateDefinitionFile() {
         path.join(__dirname, "../../src/typings/index.d.ts")
     ], { showDebugMessages: true }).getFile("main.ts");
 
-    fileInfo.getExports().forEach(def => {
-        // todo: once typescript supports it type-wise, this should be merged into one if statement
-        if (def.isClassDefinition()) {
-            def.methods = def.methods.filter(m => m.name !== "addDefinitions" &&
-                                         m.name.indexOf("fill") === -1 &&
-                                         m.name !== "addType");
-            def.properties = def.properties.filter(p => p.name.indexOf("fill") === -1 && p.name !== "addType");
-        }
-        else if (def.isInterfaceDefinition()) {
-            def.methods = def.methods.filter(m => m.name !== "addDefinitions" &&
-                                         m.name.indexOf("fill") === -1 &&
-                                         m.name !== "addType");
-            def.properties = def.properties.filter(p => p.name.indexOf("fill") === -1 && p.name !== "addType");
-        }
-    });
-
     const definitionFileText = fileInfo.writeExportsAsDefinitionFile({
         imports: [{
             defaultImport: "CodeBlockWriter",
