26fc80b11551c8ec1559fe01648e78166821a636
David Sherret <dsherret@gmail.com>
2016-07-30 11:23:55 -0400

    Slight improvement to applyMixins.

--- src/utils/applyMixins.ts
+++ src/utils/applyMixins.ts
@@ -1,11 +1,9 @@
 export function applyMixins(derivedCtor: any, extendsClass: any, baseCtors: any[]) {
     baseCtors.forEach(baseCtor => {
         Object.getOwnPropertyNames(baseCtor.prototype).forEach(name => {
-            if (name !== "constructor") {
-                if (derivedCtor.prototype[name] == null) {
-                    const descriptor = Object.getOwnPropertyDescriptor(baseCtor.prototype, name);
-                    Object.defineProperty(derivedCtor.prototype, name, descriptor);
-                }
+            if (name !== "constructor" && derivedCtor.prototype[name] == null) {
+                const descriptor = Object.getOwnPropertyDescriptor(baseCtor.prototype, name);
+                Object.defineProperty(derivedCtor.prototype, name, descriptor);
             }
         });
     });
