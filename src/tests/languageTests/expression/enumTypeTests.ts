b22c17edb0b197015d9b485b61f0e70cf3180184
David Sherret <dsherret@gmail.com>
2016-09-18 12:24:43 -0400

    #182 - Add tests

---
+++ src/tests/languageTests/expression/enumTypeTests.ts
@@ -0,0 +1,53 @@
+﻿import {getInfoFromString} from "./../../../main";
+import {runFileDefinitionTests} from "./../../testHelpers";
+
+describe("enum type tests", () => {
+    const code = `
+enum MyEnum {
+    Value
+}
+let e: MyEnum;
+class MyClass {
+    myProperty: MyEnum;
+}
+`;
+
+    const def = getInfoFromString(code);
+
+    runFileDefinitionTests(def, {
+        enums: [{
+            name: "MyEnum",
+            members: [{
+                name: "Value",
+                value: 0
+            }]
+        }],
+        variables: [{
+            name: "e",
+            type: {
+                text: "MyEnum",
+                definitions: [{
+                    name: "MyEnum"
+                }],
+                allDefinitions: [{
+                    name: "MyEnum"
+                }]
+            }
+        }],
+        classes: [{
+            name: "MyClass",
+            properties: [{
+                name: "myProperty",
+                type: {
+                    text: "MyEnum",
+                    definitions: [{
+                        name: "MyEnum"
+                    }],
+                    allDefinitions: [{
+                        name: "MyEnum"
+                    }]
+                }
+            }]
+        }]
+    });
+});
