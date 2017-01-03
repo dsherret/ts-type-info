fa51b8b7c648cf0f8b5013ce8a6c3cb388376602
michaelneu <jytho@web.de>
2017-01-03 23:03:55 +0100

    #221 - Add multiline variable declarations

--- src/tests/languageTests/variable/variableTests.ts
+++ src/tests/languageTests/variable/variableTests.ts
@@ -10,8 +10,21 @@ describe("variable tests", () => {
 var myImplicitAny;
 var myExplicitTypeVar: number;
 var myImplicitTypeVar = "my string";
+var myMultilineImplicitAny1,
+    myMultilineImplicitAny2,
+    myMultilineImplicitAny3;
+var myMultilineExplicitTypeVar1: boolean,
+    myMultilineExplicitTypeVar2: number;
+var myMultilineImplicitTypeVar1 = "my string",
+    myMultilineImplicitTypeVar2 = 42;
+
 let myLet: string;
+let myMultilineLet1: number,
+    myMultilineLet2;
+
 const myConst: number;
+const myMultilineConst1 = 10,
+      myMultilineConst2: string;
 `;
 
     const def = getInfoFromString(code);
@@ -32,13 +45,60 @@ const myConst: number;
             type: { text: "string" },
             defaultExpression: { text: `"my string"` }
         }, {
+            name: "myMultilineImplicitAny1",
+            declarationType: VariableDeclarationType.Var,
+            type: { text: "any" }
+        }, {
+            name: "myMultilineImplicitAny2",
+            declarationType: VariableDeclarationType.Var,
+            type: { text: "any" }
+        }, {
+            name: "myMultilineImplicitAny3",
+            declarationType: VariableDeclarationType.Var,
+            type: { text: "any" }
+        }, {
+            name: "myMultilineExplicitTypeVar1",
+            declarationType: VariableDeclarationType.Var,
+            type: { text: "boolean" }
+        }, {
+            name: "myMultilineExplicitTypeVar2",
+            declarationType: VariableDeclarationType.Var,
+            type: { text: "number" }
+        }, {
+            name: "myMultilineImplicitTypeVar1",
+            declarationType: VariableDeclarationType.Var,
+            type: { text: "string" },
+            defaultExpression: { text: `"my string"` }
+        }, {
+            name: "myMultilineImplicitTypeVar2",
+            declarationType: VariableDeclarationType.Var,
+            type: { text: "number" },
+            defaultExpression: { text: "42" }
+        }, {
             name: "myLet",
             declarationType: VariableDeclarationType.Let,
             type: { text: "string" }
         }, {
+            name: "myMultilineLet1",
+            declarationType: VariableDeclarationType.Let,
+            type: { text: "number" }
+        }, {
+            name: "myMultilineLet2",
+            declarationType: VariableDeclarationType.Let,
+            type: { text: "any" }
+        }, {
             name: "myConst",
             declarationType: VariableDeclarationType.Const,
             type: { text: "number" }
+        }, {
+            name: "myMultilineConst1",
+            declarationType: VariableDeclarationType.Const,
+            type: { text: "number" },
+            defaultExpression: { text: "10" }
+        }, {
+            name: "myMultilineConst2",
+            declarationType: VariableDeclarationType.Const,
+            type: { text: "string" }
         }]
     });
 });
